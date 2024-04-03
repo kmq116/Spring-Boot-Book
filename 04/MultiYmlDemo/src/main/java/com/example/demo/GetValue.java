796d17dd4578b45d8c73b73f75bc9c18d36d96bd
飞天御剑流co <521luqiya@gmail.com>
2024-04-03 17:55:40 +0800

    自己编写个接口，调用配置文件中的变量

---
+++ 04/MultiYmlDemo/src/main/java/com/example/demo/GetValue.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class GetValue {
+    //获取配置文件中的age
+    @Value("${myenvironment.name}") // 赋值初始值
+    private String myenvironment;
+    @GetMapping("/getValue")
+    private String getMyenvironment() {
+        return myenvironment;
+    }
+
+
+}
